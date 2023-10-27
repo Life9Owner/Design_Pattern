ad3936b8b120d87372c7d6a7851b6fc91fa38788
Life9Owner <1349615576@qq.com>
2023-10-27 17:18:12 +0800

    comment

--- mediator.java
+++ mediator.java
@@ -1,4 +1,7 @@
-
+/*Define an object that encapsulates how a set of objects interact.
+Mediator promotes loose coupling by keeping objects from 
+referring to each other explicitly,
+and it lets you vary their interaction independently. */
 interface WebServiceMediator{
     // public (1);
     public void buy(double money,WebService service);
